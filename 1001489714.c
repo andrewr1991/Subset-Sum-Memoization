c6e5e2ac165dfdca35ab3bffcb97a6b90604b504
Frank <Fridout106@gmail.com>
2017-03-22 13:19:18 -0500

    Second build of subset sum. Code taken from Bob Weems

--- 1001489714.c
+++ 1001489714.c
@@ -1,76 +1,94 @@
+// Subset sums by dynamic programming
+// CSE 2320 Notes 7
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void readInput(int* n, int* m, int** S) {
-  // Loads all input and allocates space
-  int i;
+void readInput(int* n,int* m,int** S,int** C) {
+    int i;
 
-  scanf("%d",n);
-  scanf("%d",m);
+    scanf("%d",n);
+    scanf("%d",m);
 
-  *S=(int*) malloc((*n+1)*sizeof(int));
-  //*C=(int*) malloc((*m+1)*sizeof(int));
+    *S=(int*) malloc((*n+1)*sizeof(int));
+    *C=(int*) malloc((*m+1)*sizeof(int));
 
-  if (!(*S)) {
-    printf("malloc failed %d\n",__LINE__);
-    exit(0);
-  }
+    if (!(*S) || !(*C)) {
+      printf("malloc failed %d\n",__LINE__);
+      exit(0);
+    }
 
-  // Sentinel zero
-  (*S)[0] = 0;
+    (*S)[0]=0;
 
-  for (i = 1; i <= *n; i++) {
-    scanf("%d", *S+i);
-  }
+    for (i = 1; i <= *n; i++) {
+      scanf("%d", *S + i);
+    }
 }
 
-void subsetSum(int n, int m, int* S) {
-  int i, k;
-  int index[m+1];
-  int C[25][7];
-  int sizeIndex = sizeof(index)/sizeof(index[0]);
+void cardSubsetSum(int n,int m,int* S,int* C) {
+    int i, j, potentialSum, leftover;
+    int card[n][m];
 
-  printf("\n");
+    C[0]=0;
 
-  for (i = 0; i < sizeIndex; i++) {
-    index[i] = i;
-  }
-
-  for (i = 0; i < sizeIndex; i++) {
-    for (k = 0; k < n; k++) {
-      if ((((index[i] - S[k]) >= 0) && ((index[i] - (S)[k]) != index[i])) && ((index[i] - S[k]) > C[i-1][k]))  {
-        //printf("%d %d\n", S[k], index[i]);
-        //printf("good\n");
-        C[i-1][k] = k;
+    for (potentialSum=1; potentialSum<=m; potentialSum ++) {
+      for (j = 1; j <= n; j++) {
+        leftover = potentialSum - S[j];
+        if (leftover>=0 && C[leftover]<j) {
+          break;
+        }
       }
-      else {
-        //printf("%d %d\n", S[k], index[i]);
-        //printf("bad\n");
-        C[i-1][k] = 8;
-      }
+      C[potentialSum]=j;
     }
-    printf("\n-----------\n");
-  }
-
-  for (i = 0; i < sizeIndex; i++) {
-    for (k = 0; k < n; k++) {
-      printf("%d ", C[i-1][k]);
-    }
-    printf("\n");
-  }
 }
 
+void writeOutput(int n,int m,int* S,int* C) {
+    int i;
+
+    printf("  i   S\n");
+    printf("-------\n");
+
+    for (i=0;i<=n;i++) {
+      printf("%3d %3d\n",i,S[i]);
+    }
+
+
+    if (m <= 50) {
+      printf("  i   C\n");
+      printf("-------\n");
+
+      for (i = 0 ;i <= m; i++) {
+        printf("%3d %3d\n",i,C[i]);
+      }
+    }
+
+
+    if (C[m] == n+1) {
+      printf("No solution\n");
+    }
+    else {
+      printf("Solution\n");
+      printf("  i   S\n");
+      printf("-------\n");
+
+      for (i = m; i > 0; i -= S[C[i]]) {
+        printf("%3d %3d\n",C[i],S[C[i]]);
+      }
+    }
+}
 
 int main() {
-  int n;    // Size of input set
-  int m;    // Target value
-  int *S;   // Input set
-  int *C;   // Cost table
+    int n;    // Size of input set
+    int m;    // Target value
+    int *S;   // Input set
+    int *C;   // Cost table
 
-  readInput(&n,&m,&S);
+    readInput(&n,&m,&S,&C);
 
-  subsetSum(n,m,S);
+    cardSubsetSum(n,m,S,C);
 
-  free(S);
-  free(C);
+    writeOutput(n,m,S,C);
+
+    free(S);
+    free(C);
 }
